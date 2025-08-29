bf71dcd21bb81b78f743179ad8bee5ec93be2bdb
yogeshsadgir0005 <yogeshsadgir05@gmail.com>
2025-08-30 01:39:50 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ import Skills from '../components/Skills';
 import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import Education from '../components/Education';
-import Contact from '../components/Contact';
+import Contact from '../components/Contact'; 
 import Footer from '../components/Footer';
 
 const Home = () => {
