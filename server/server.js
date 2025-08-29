e73d29f94371be11776ac009ce9542a9d6447266
yogeshsadgir0005 <yogeshsadgir05@gmail.com>
2025-08-30 01:00:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,8 @@ app.use((err, req, res, next) => {
 });
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/yogesh-portfolio'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://yogeshsadgir05:Sadgir0520@cluster0.6ws43d6.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0
+'
 })
 .then(() => {
   console.log('Connected to MongoDB');
